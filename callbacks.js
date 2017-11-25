9e1da56f0d04c3b8f817033d1c9042594de77235
sedatiko <sedatbehar@gmail.com>
2017-11-25 15:37:48 -0500

    update callbacks

--- callbacks.js
+++ callbacks.js
@@ -1,9 +1,27 @@
-var handleResults = function(error, results){
-    // if error is undefined
-    // do something with the results
-}
+var maxTime = 1000;
 
-// use anon as callback when a simple, one-time call is made
-getStuff(function(err, stuffToGet){
+var evenDoubler = function(v, callback){
+    var waitTime = Math.floor(Math.random()*(maxTime+1));
 
-});
+    if(v % 2) {
+        setTimeout(function(){
+            callback(new Error("Odd Input"));
+        }, waitTime);
+    }else{
+        setTimeout(function(){
+            callback(null, v*2, waitTime);
+        }, waitTime);
+    }
+
+};
+
+var handleResults = function (err, results, time){
+    if (err){
+        console.log("ERROR: " + err.message);
+    }else{
+        console.log("Results: " + results + " ("+time+ ") ms");
+    }
+};
+
+evenDoubler(2, handleResults);
+evenDoubler(7, handleResults);
